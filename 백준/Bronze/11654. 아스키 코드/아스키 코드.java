35c45a3fe329113a6fcd2cf696eb058db4a37ac8
Goeun Moon <148797341+moongzz@users.noreply.github.com>
2024-01-14 14:17:14 +0900

    [Bronze V] Title: 아스키 코드, Time: 120 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int input = br.read();
+		System.out.print(input);
+		br.close();
+	}
+}
