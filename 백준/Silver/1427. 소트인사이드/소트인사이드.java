775507df0430ad7e6d81d7314fceda426a965674
Goeun Moon <148797341+moongzz@users.noreply.github.com>
2025-05-30 22:46:31 +0900

    [Silver V] Title: 소트인사이드, Time: 96 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int[] arr = new int[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            arr[i] = Integer.parseInt(str.substring(i, i + 1));
+        }
+        for (int i = 0; i < str.length(); i++) {
+            int max = i;
+            for (int j = i + 1; j < str.length(); j++) {
+                if (arr[j] > arr[max]) max = j;
+            }
+            if (arr[i] < arr[max]) {
+                int temp = arr[i];
+                arr[i] = arr[max];
+                arr[max] = temp;
+            }
+        }
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(arr[i]);
+        }
+    }
+}
